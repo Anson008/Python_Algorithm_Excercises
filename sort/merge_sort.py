472d95f366c8c85ab0662c10da61256afc3c6744
Xingchen Zhao <xczhao007@gmail.com>
2020-11-06 15:38:57 -0600

    Refactor merge sort

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -1,27 +1,36 @@
-def merge(array1, array2):
-    i = j = 0
-    result = []
-    while i < len(array1) and j < len(array2):
-        if array1[i] < array2[j]:
-            result.append(array1[i])
+class Solution(object):
+    def merge_sort(self, array):
+        """
+        input: int[] array
+        return: int[]
+        """
+        # write your solution here
+        if len(array) == 0 or len(array) == 1:
+            return array
+        mid = len(array) // 2
+        left = self.merge_sort(array[:mid])
+        right = self.merge_sort(array[mid:])
+        return self._merge(left, right)
+
+    @staticmethod
+    def _merge(arr1, arr2):
+        i, j = 0, 0
+        res = []
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] <= arr2[j]:
+                res.append(arr1[i])
+                i += 1
+            else:
+                res.append(arr2[j])
+                j += 1
+        while i < len(arr1):
+            res.append(arr1[i])
             i += 1
-        else:
-            result.append(array2[j])
+        while j < len(arr2):
+            res.append(arr2[j])
             j += 1
-    if i < len(array1):
-        result.extend(array1[i:])
-    if j < len(array2):
-        result.extend(array2[j:])
-    return result
+        return res
 
-
-def merge_sort(array):
-    if len(array) == 0 or len(array) == 1:
-        return array
-    middle = len(array) // 2
-    left = merge_sort(array[:middle])
-    right = merge_sort(array[middle:])
-    return merge(left, right)
 
 # time: O(n*log(n))
 # space: O(n)
@@ -30,10 +39,11 @@ def merge_sort(array):
 if __name__ == "__main__":
     a1 = [3, 27, 38, 43]
     a2 = [3, 10, 82]
-    m1 = merge(a1, a2)
-    print("Merged array:", m1)
-
     a3 = [4, 1, 12, 45, 98, 12, 23]
-    print("Unsorted:", a3)
-    a3_sorted = merge_sort(a3)
-    print("Sorted:", a3_sorted)
+
+    test = [a1, a2, a3]
+    solu = Solution()
+
+    for arr in test:
+        print("Original: ", arr)
+        print("Sorted: ", solu.merge_sort(arr))
