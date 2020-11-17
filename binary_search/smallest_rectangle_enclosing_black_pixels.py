b874869bb931576d29cf4067e3a0e6e7562a9f10
Xingchen <xczhao007@gmail.com>
2020-11-17 10:48:31 -0600

    Add "smallest_rectangle_enclosing_black_pixels.py"

---
+++ binary_search/smallest_rectangle_enclosing_black_pixels.py
@@ -0,0 +1,72 @@
+class Solution(object):
+    def minArea(self, image, x, y):
+        """
+        input: int[][] image, int x, int y
+        return: int
+        """
+        # write your solution here
+        num_rows = len(image)
+        num_cols = len(image[0])
+        if num_rows == 0 and num_cols == 0:
+            return 0
+        top = self.search_vertically(image, 0, x)
+        bottom = self.search_vertically(image, x + 1, num_rows - 1, False)
+        left = self.search_horizontally(image, 0, y)
+        right = self.search_horizontally(image, y + 1, num_cols - 1, False)
+        return (bottom - top) * (right - left)
+
+    @staticmethod
+    def search_vertically(image, left, right, top=True):
+        while left < right:
+            mid = (left + right) // 2
+            if top:
+                if 1 in image[mid]:
+                    right = mid
+                else:
+                    left = mid + 1
+            else:
+                if 1 in image[mid]:
+                    left = mid + 1
+                else:
+                    right = mid
+        return left
+
+    @staticmethod
+    def search_horizontally(image, left, right, top=True):
+        while left < right:
+            mid = (left + right) // 2
+            #print("1:", mid)
+            flag = False
+            for p in image:
+                #print(p)
+                if p[mid] == 1:
+                    flag = True
+                    break
+            if top:
+                if flag is True:
+                    right = mid
+                    #print("2:", mid)
+                else:
+                    left = mid + 1
+                    #print("3:", mid)
+            else:
+                if flag is True:
+                    left = mid + 1
+                else:
+                    right = mid
+        return left
+
+
+if __name__ == "__main__":
+    m1 = [[0, 0, 1, 0], [0, 1, 1, 0], [0, 1, 0, 0]]
+    num_rows = len(m1)
+    num_cols = len(m1[0])
+    p = (0, 2)
+
+    solu = Solution()
+    print(solu.search_vertically(m1, 0, p[0]))
+    print(solu.search_vertically(m1, p[0], num_rows - 1, False))
+    print(solu.search_horizontally(m1, 0, p[1]))
+    print(solu.search_horizontally(m1, p[1], num_cols - 1, False))
+
+
