8a99b5ebcd9828e51ac8fca52e9f4e0cc1435157
Xingchen Zhao <xczhao007@gmail.com>
2020-11-24 15:20:08 -0600

    Implement "dedup4" for deduplicating unsorted array in "array_deduplication.py"

--- array/array_deduplication.py
+++ array/array_deduplication.py
@@ -53,15 +53,33 @@ class Solution(object):
             i += 1
         return array[:j]
 
+    def dedup4(self, array):
+        n = len(array)
+        if n <= 1:
+            return array
+        i, j = 0, 1
+        while j <= n - 1:
+            if i >= 0 and array[j] == array[i]:
+                while j <= n - 1 and array[j] == array[i]:
+                    j += 1
+                i -= 1
+            else:
+                array[i + 1] = array[j]
+                j += 1
+                i += 1
+        return array[:i + 1]
+
 
 if __name__ == "__main__":
     a1 = [1, 2, 2, 2, 3, 3, 3, 3]
     a2 = [1, 2, 3]
+    a3 = [1, 2, 2, 3, 3, 3, 2, 2]
     solu = Solution()
     print(solu.dedup2(a1))
     print(solu.dedup2(a2))
     print(solu.dedup3(a1))
     print(solu.dedup3(a2))
+    print(solu.dedup3(a3))
 
 
 
